421c922fa99521d7abf6c26a308eb55095c11c41
longhehe21 <htl20122003@gmail.com>
2025-12-12 15:56:16 +0700

    ddxxx

--- core/settings.py
+++ core/settings.py
@@ -136,8 +136,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 # Static & Media
 
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # <-- DÒNG QUAN TRỌNG
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
